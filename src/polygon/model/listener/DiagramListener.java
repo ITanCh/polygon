3153a328a054003828c54b7789dd4d9b1592f38c
ITanCh <liutianchi92@gmail.com>
2014-03-03 22:21:07 +0800

    PropertyChangeListener的加入，使代码更正宗

--- src/polygon/model/listener/DiagramListener.java
+++
@@ -1,7 +0,0 @@
-package polygon.model.listener;
-
-import polygon.model.PolyPoint;
-
-public interface DiagramListener {
-	void addChild(PolyPoint p);
-}
--- src/polygon/model/listener/ElementListener.java
+++
@@ -1,6 +0,0 @@
-package polygon.model.listener;
-
-public interface ElementListener {
-	void changeLocation(int x,int y);
-	void changeConnection();
-}
